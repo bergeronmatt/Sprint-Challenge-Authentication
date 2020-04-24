b46340f61ce49ba02b404d87bc49d08532844b22
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-24 11:41:00 -0400

    coded authenticate-middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,27 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../api/secrets');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization;
+
+  const secret = secrets.jwtSecret;
+
+  if (token) {
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ errorMessage: 'Invalid Token' })
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
